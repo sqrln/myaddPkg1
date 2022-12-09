94c3d76e70d4a263d9a4a77c7af9a8ff62c8e127
sqrln <3574327970@qq.com>
2022-12-09 16:46:55 +0800

    v0.3

---
+++ R/add1.R
@@ -0,0 +1,17 @@
+#' 加法
+#'
+#' @param x 参数x
+#' @param y 参数y
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' a=myadd()
+#' b=myadd(3,4)
+#' c=myadd(x=5,y=6)
+#' d=myadd(y=2,x=3)
+myadd <- function(x=1,y=2) {
+  res=x+y
+  return(res)
+}
